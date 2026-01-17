57f409276bf819b87e7ab0a93ec74dd12d829131
Yunsang-K <dbstkd0098@naver.com>
2026-01-17 15:35:19 +0900

    Refactor allowedHosts configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-
 export default defineConfig({
+  plugins: [vue()],
   server: {
-    allowedHosts: [
-      '.up.railway.app',
-    ],
+    allowedHosts: ['.up.railway.app'],
+  },
+  preview: {
+    allowedHosts: ['.up.railway.app'],
   },
 })
